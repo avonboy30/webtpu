ac6b30d594760a4d1f984c3bbf90c2c5b17c7e1d
avonboy30 <102927829+avonboy30@users.noreply.github.com>
2024-10-26 18:31:10 +0800

    v1

---
+++ dist/nono.js
@@ -0,0 +1,13 @@
+$(function() {
+  const removeWarning = () => {
+    const el = document.querySelector('#warning');
+    if (el) {
+      el.style.display = 'none';
+    } else {
+      setTimeout(removeWarning, 100);
+    }
+  };
+
+  removeWarning();
+
+});
